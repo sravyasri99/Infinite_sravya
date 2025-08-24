4387dfe2fb18de485350e581bd3be628e4005925
Sravyasri Ommi <sravyasrio@infinite.com>
2025-08-24 18:10:16 +0530

    MVC_Assignment

---
+++ MVC/Assignments/MVC_Assignment1/MVC_Assignment1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Assignment1.MvcApplication" Language="C#" %>
---
+++ MVC/Assignments/MVC_Assignment1/MVC_Assignment1/Repository/IContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVC_Assignment1.Models;
+
+namespace MVC_Assignment1.Repository
+{
+    interface IContactRepository
+    {
+        Task<List<Contact>> GetAllAsync();
+        Task CreateAsync(Contact contact);
+        Task DeleteAsync(long Id);
+    }
+}
